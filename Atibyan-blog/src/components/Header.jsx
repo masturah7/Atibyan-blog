61c429ab9f4814c3fb38a922ca4b1ef13330aacb
Olabanj <olabanjolukman5@gmail.com>
2024-12-12 16:37:33 +0100

    Header complete

--- Atibyan-blog/src/components/Header.jsx
+++ Atibyan-blog/src/components/Header.jsx
@@ -1,14 +1,30 @@
-import { Button, Navbar } from "flowbite-react";
-
+import { Button, Navbar, TextInput } from "flowbite-react";
+import {FaMoon} from "react-icons/fa"
+import{AiOutlineSearch} from "react-icons/ai"
+import{ Link} from "react-router-dom"
 export default function Header() {
   return (
-    <Navbar fluid rounded>
-      <Navbar.Brand href="https://flowbite-react.com">
-        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+    <Navbar fluid rounded className="border-b-2">
+      <Navbar.Brand href="https://flowbite-react.com" className="self-center whitespace-nowrap text-xl sm:text-xl font-semibold dark:text-white" >
+        
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-pink-500 to-blue-500 rounded-lg text-white">Atibyan</span>Blog
       </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Button>Get started</Button>
+      <form>
+        <TextInput placeholder="search...." rightIcon={AiOutlineSearch} className="hidden lg:inline" />
+      </form>
+
+
+      <div className="flex  gap-2 md:order-2">
+      <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+        <FaMoon />
+         </Button>
+      
+       <Link>
+        <Button gradientDuoTone="purpleToPink" outline>
+          Sign Up
+          </Button>
+        </Link>
+        
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
