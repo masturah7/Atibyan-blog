9c58138ffeb118edb867ac3e7b9aeded66c4282c
Olabanj <olabanjolukman5@gmail.com>
2024-12-12 14:52:25 +0100

    components creation with header

--- Atibyan-blog/src/components/Header.jsx
+++ Atibyan-blog/src/components/Header.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Button, Navbar } from "flowbite-react";
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <Navbar fluid rounded>
+      <Navbar.Brand href="https://flowbite-react.com">
+        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Button>Get started</Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="/" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="/about">About</Navbar.Link>
+        <Navbar.Link href="/projects">Projects</Navbar.Link>
+        
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
