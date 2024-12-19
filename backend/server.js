acf4e0d946a3d12fd7419a8d2f5438ba9c2cec7d
Olabanj <olabanjolukman5@gmail.com>
2024-12-19 19:40:31 +0100

    Database connecting

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config();
 
-
+mongoose.connect(process.env.MONGO).then(
+    () => {console.log('Mongodb is connected successfully')}
+)
+.catch((err) => {
+    console.log(err)
+});
 const app = express();
 
 app.listen(3000, ()=>{
